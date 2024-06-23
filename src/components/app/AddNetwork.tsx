862bf4e971692385f4a7f053307c7dd47ff70942
Benskalz <22433963+Benskalz@users.noreply.github.com>
2024-06-23 05:18:06 +0200

    feat: add network from url params

--- src/components/app/AddNetwork.tsx
+++ src/components/app/AddNetwork.tsx
@@ -1,75 +1,77 @@
-import { BiReceipt } from "react-icons/bi";
 import { networks } from "../../utils/networks";
-import { SlArrowDownCircle, SlArrowUp, SlArrowUpCircle } from "react-icons/sl";
-import { AiOutlineSwap } from "react-icons/ai";
 import {
-  Button,
-  ErrorBlock,
-  Form,
-  Input,
-  Modal,
-  NavBar,
-  NoticeBar,
-  ResultPage,
-  Skeleton,
-  Space,
-  Toast,
+  Button, Form,
+  Input, NavBar,
+  NoticeBar, Toast
 } from "antd-mobile";
-import { useEffect, useState } from "react";
-import Receive from "./Receive";
-import { QRCodeSVG } from "qrcode.react";
-import { CopyToClipboard, getAccount } from "../Settings";
-import Send from "./Send";
-import History from "./History";
-import useSWR from "swr";
-import { getWalletRPC, rawToMega } from "../../nano/accounts";
-import RPC from "../../nano/rpc";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { fetchPrices } from "./Home";
-import { GoCreditCard } from "react-icons/go";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
-export default function AddNetwork({  }) {
-  const {  } = useParams();
+export default function AddNetwork({ }) {
+
+  const [searchParams] = useSearchParams();
+
   const navigate = useNavigate();
-  const router = useLocation();
   return (
     <div className="">
       <div className="container  relative mx-auto">
         <div className="text-center text-2xl flex-col">
           <NavBar onBack={() => navigate("/")}>Add custom nano network</NavBar>
-         </div>
-         <NoticeBar className="mb-2 text-sm" wrap content="Anyone can create a custom network, including malicious ones. Add only trusted networks."
-          color="alert"/>
-         <Form onFinish={(values) => {
+        </div>
+        <NoticeBar className="mb-2 text-sm" wrap content="Anyone can create a custom network, including malicious ones. Add only trusted networks."
+          color="alert" />
+        <Form
+          initialValues={{
+              name: searchParams.get("name") || "",
+              ticker: searchParams.get("ticker") || "",
+              rpc: searchParams.get("rpc") || "",
+              decimals: searchParams.get("decimals") || "",
+              prefix: searchParams.get("prefix") || "",
+            }}
+          onFinish={(values) => {
             console.log(values);
+            if (networks[values.ticker]) {
+              Toast.show({
+                icon: 'fail',
+                content: `Network ${values.ticker} already exists.`
+              });
+              return;
+            }
             let newCustomNetworks = JSON.parse(localStorage.getItem("customNetworks"));
             if (!newCustomNetworks) {
               newCustomNetworks = {};
             }
             newCustomNetworks[values.ticker] = values;
+            newCustomNetworks[values.ticker].decimals = parseInt(values.decimals);
             newCustomNetworks[values.ticker].logo = window.location.origin + "/public/img/crypto/unknown.svg";
+            newCustomNetworks[values.ticker].custom = true;
+
             localStorage.setItem("customNetworks", JSON.stringify(newCustomNetworks));
+            Toast.show({
+              icon: 'success',
+              content: `Network ${values.ticker} added.`
+            });
+            navigate("/");
           }}>
-            <Form.Item label="Name" name={"name"} required={false} rules={[{ required: true, message: "Please enter a name" }]}>
-              <Input placeholder="MyNano" />
-            </Form.Item>
-            <Form.Item label="Ticker" name={"ticker"} required={false} rules={[{ required: true, message: "Please enter a ticker" }]}>
-              <Input placeholder="MXNO" />
-            </Form.Item>
-            <Form.Item label="RPC URL" name={"rpc"} required={false} rules={[{ required: true, message: "Please enter a RPC URL" }]}>
-              <Input placeholder="http://localhost:4242" />
-            </Form.Item>
-            <Form.Item label="Decimals" name={"decimals"} required={false} rules={[{ required: true, message: "Please enter a decimals" }]}>
-              <Input placeholder="30" />
-            </Form.Item>
-            <Form.Item label="Prefix" name={"prefix"} required={false} rules={[{ required: true, message: "Please enter a prefix" }]}>
-              <Input placeholder="nani" />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary">Add network</Button>
-            </Form.Item>
-         </Form>
-         </div>
+          <Form.Item label="Name" name={"name"} required={false} rules={[{ required: true, message: "Please enter a name" }]}>
+            <Input placeholder="MyNano" />
+          </Form.Item>
+          <Form.Item label="Ticker" name={"ticker"} required={false} rules={[{ required: true, message: "Please enter a ticker" }]}>
+            <Input placeholder="MXNO" />
+          </Form.Item>
+          <Form.Item label="RPC URL" name={"rpc"} required={false} rules={[{ required: true, message: "Please enter a valid RPC URL", type: "url" }]}>
+            <Input placeholder="http://localhost:4242" />
+          </Form.Item>
+          <Form.Item label="Decimals" name={"decimals"} required={false} rules={[{ required: true, message: "Please enter a decimals" }]}>
+            <Input placeholder="30" />
+          </Form.Item>
+          <Form.Item label="Prefix" name={"prefix"} required={false} rules={[{ required: true, message: "Please enter a prefix" }]}>
+            <Input placeholder="nani" />
+          </Form.Item>
+          <Form.Item>
+            <Button color="primary" type="submit">Add network</Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 }
