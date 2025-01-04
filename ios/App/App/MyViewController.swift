860634a1fa75f629455142c54d99632142cc9976
ben <ben@nanwallet.com>
2025-01-04 19:56:39 +0100

    native ios

---
+++ ios/App/App/MyViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+import Capacitor
+
+class MyViewController: CAPBridgeViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView!.allowsBackForwardNavigationGestures = true
+    }
+}
